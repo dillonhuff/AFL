70cb4cb4f145a08e7b60a1ebdd8b16f819c78948
Dillon Huff <dillonhuff@gmail.com>
2014-10-05 16:14:00 -0500

    Main.hs now prints out results of lexing and parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,18 @@
 module Main(main) where
 
-import Language.Haskell.Exts.Parser
 import System.Environment
 import System.IO
+
+import Lexer
+import Parser
 
 main = do
   (fileName:rest) <- getArgs
   fileHandle <- openFile fileName ReadMode
   programText <- hGetContents fileHandle
-  let parsedMod = parseModule programText
+  putStrLn $ show programText
+  let toks = strToToks programText
+  putStrLn $ show toks
+  let parsedMod = parseExpr $ strToToks programText
   hClose fileHandle
   putStrLn $ show parsedMod
